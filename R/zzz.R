d4d76ece9954a15e7c97e92eca0c2c0b252e62de
baayen <baayen@ualberta.ca>
2011-04-23 00:00:00 +0000

    version 1.2

--- R/zzz.R
+++ R/zzz.R
@@ -3,6 +3,7 @@
 cat("attaching languageR\n")
 
 require(methods, quietly = TRUE)
+#require(lme4, quietly=TRUE)
 
 #setMethod("show", "corres", function(object) print.corres(object))
 #setMethod("show", "growth", function(object) show.growth(object))
